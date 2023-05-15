29edf152032c93233bdfa5fb3d3c847901fda90a
grace kurui <gracekurui123@gmail.com>
2023-05-15 15:24:00 +0300

    test run

---
+++ app.js
@@ -0,0 +1,24 @@
+// function calculateTotal(r) {
+//   const pi = 3.14;
+//   var area = pi * r * r;
+//   console.log(area);
+// }
+// areaofcircle(82);
+let dice = 10;
+switch (dice) {
+  case 1:
+    console.log(`you've diced 1`);
+    break;
+  case 2:
+    console.log(`you've diced 2`);
+    break;
+  case 3:
+    console.log(`you've diced 3`);
+    break;
+  case 4:
+    console.log(`you've diced 4`);
+    break;
+  default:
+    console.log(`invalid`);
+  //comment
+}
